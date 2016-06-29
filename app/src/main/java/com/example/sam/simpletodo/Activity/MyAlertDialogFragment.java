329e101d31153edc742fd177e7d59e145f1c8393
Yu-Shiuan Chu <mz0401sam.ee96@gmail.com>
2016-06-29 03:19:39 -0700

    Add priority and Dialog Fragment

---
+++ app/src/main/java/com/example/sam/simpletodo/Activity/MyAlertDialogFragment.java
@@ -0,0 +1,75 @@
+package com.example.sam.simpletodo.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
+import com.example.sam.simpletodo.R;
+
+
+public class MyAlertDialogFragment extends DialogFragment implements OnEditorActionListener{
+    private EditText mEditText;
+
+    public interface EditNameDialogListener {
+        void onFinishEditDialog(String inputText);
+    }
+    public MyAlertDialogFragment() {
+        // Empty constructor is required for DialogFragment
+        // Make sure not to add arguments to the constructor
+        // Use `newInstance` instead as shown below
+    }
+
+    public static MyAlertDialogFragment newInstance(String title) {
+        MyAlertDialogFragment frag = new MyAlertDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mEditText.setOnEditorActionListener(this);
+        return inflater.inflate(R.layout.fragment_edit_name, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Get field from view
+        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
+        // Fetch arguments from bundle and set title
+        String title = getArguments().getString("title", "Enter Name");
+        getDialog().setTitle(title);
+        // Show soft keyboard automatically and request focus to field
+        mEditText.requestFocus();
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
+
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (EditorInfo.IME_ACTION_DONE == actionId) {
+            // Return input text back to activity through the implemented listener
+            EditNameDialogListener listener = (EditNameDialogListener) getActivity();
+            listener.onFinishEditDialog(mEditText.getText().toString());
+            // Close the dialog and return back to the parent activity
+            dismiss();
+            return true;
+        }
+        return false;
+    }
+
+
+}
